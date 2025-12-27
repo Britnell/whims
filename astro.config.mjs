2f046d536545e1e431665209031162d0f95db029
Britnell <t.a.britnell@gmail.com>
2025-12-27 20:00:10 +0100

    github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://britnell.github.io',
+  base: '/whims',
   vite: {
     plugins: [tailwindcss()]
   }
